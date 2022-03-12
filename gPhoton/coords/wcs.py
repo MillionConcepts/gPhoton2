8445ff1201c8eb441bc4de4898fc0f6bb8dd69e1
mstclair@millionconcepts.com <mstclair@millionconcepts.com>
2022-03-12 15:26:25 -0500

    scale image size to projection at high dec; handle ra at 360 wraparound better

--- gPhoton/coords/wcs.py
+++ gPhoton/coords/wcs.py
@@ -37,14 +37,25 @@ def make_bounding_wcs(radec: np.ndarray) -> astropy.wcs.WCS:
     """
     makes a WCS solution for a given range of ra/dec values,
     at the constant degree-per-pixel scale set in gPhoton.constants.DEGPERPIXEL
+    assumes gnomonically-projected ra/dec values; scales ra bounds to
+    approximate distortion in pixel size
     """
+    import math
     real_ra = radec[:, 0][np.isfinite(radec[:, 0])]
     real_dec = radec[:, 1][np.isfinite(radec[:, 1])]
     ra_range = real_ra.min(), real_ra.max()
     dec_range = real_dec.min(), real_dec.max()
-    center_skypos = (np.mean(ra_range), np.mean(dec_range))
+    # viewports in which ra wraps around 360
+    if ra_range[1] - ra_range[0] > 350:
+        real_ra[real_ra > 180] -= 360
+        ra_range = real_ra.min(), real_ra.max()
+    # WCS center pixel in sky coordinates
+    ra0, dec0 = (np.mean(ra_range), np.mean(dec_range))
+    ra0 = ra0 if ra0 > 0 else ra0 + 360
+    # scale by approximate ra-direction distortion of gnomonic projection
+    ra_offset = (ra_range[1] - ra_range[0]) * math.cos(math.radians(dec0))
     imsz = (
         int(np.ceil((dec_range[1] - dec_range[0]) / c.DEGPERPIXEL)),
-        int(np.ceil((ra_range[1] - ra_range[0]) / c.DEGPERPIXEL)),
+        int(np.ceil(ra_offset / c.DEGPERPIXEL)),
     )
-    return make_wcs(center_skypos, imsz=imsz, pixsz=c.DEGPERPIXEL)
+    return make_wcs((ra0, dec0), imsz=imsz, pixsz=c.DEGPERPIXEL)
