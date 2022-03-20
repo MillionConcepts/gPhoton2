38d23fe9bdc6462dd7ff7a3e3569bf8f8119869f
mstclair@millionconcepts.com <mstclair@millionconcepts.com>
2022-03-20 19:12:21 -0400

    add req, allow quiet watch

--- gPhoton/reference.py
+++ gPhoton/reference.py
@@ -56,16 +56,18 @@ class Stopwatch(FakeStopwatch):
     simple timer object
     """
 
-    def __init__(self, digits=2):
+    def __init__(self, digits=2, silent=False):
         self.digits = digits
         self.last_time = None
         self.start_time = None
+        self.silent = silent
 
     def peek(self):
         return round(time() - self.last_time, self.digits)
 
     def start(self):
-        print("starting timer")
+        if self.silent is False:
+            print("starting timer")
         now = time()
         self.start_time = now
         self.last_time = now
@@ -73,5 +75,6 @@ class Stopwatch(FakeStopwatch):
     def click(self):
         if self.last_time is None:
             return self.start()
-        print(f"{self.peek()} elapsed seconds, restarting timer")
+        if self.silent is False:
+            print(f"{self.peek()} elapsed seconds, restarting timer")
         self.last_time = time()
