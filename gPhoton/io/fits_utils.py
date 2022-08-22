96a799444f48951b0cf0ed526906b2e5de16337b
michael <mstclair@millionconcepts.com>
2022-08-22 10:46:24 -0400

    cut logged initializer for now, mess with skimmer for rice files

--- gPhoton/io/fits_utils.py
+++ gPhoton/io/fits_utils.py
@@ -129,6 +129,8 @@ def first_fits_header(path: Pathlike, header_records: int = 1):
         stream = igzip.open(path)
     else:
         stream = open(path, "rb")
+    if "rice" in str(path):
+        stream.seek(2880)
     head = head_file(stream, 2880 * header_records)
     stream.close()
     with warnings.catch_warnings():
@@ -161,63 +163,3 @@ def get_header(hdul: Sequence, hdu_ix: int, library: str):
     raise ValueError(f"don't know {library}")
 
 
-def logged_fits_initializer(
-    path: Pathlike,
-    loader: Callable,
-    hdu_indices: Sequence[int],
-    get_wcs: bool = False,
-    get_handles: bool = False,
-    verbose: int = 0,
-    logged: bool = True,
-    astropy_handle_attribute: str = "data"
-):
-    """
-    initialize a FITS object using a passed 'loader' -- probably
-    astropy.io.fits.open, a constructor for fitsio.FITS, or a wrapped
-    version of one of those. optionally also meticulously record time and
-    network transfer involved at all stages of its initialization. At
-    present, this function is primarily used for benchmarking.
-    """
-    # initialize fits HDU list object and read selected HDU's header
-    stat, note = make_monitors(fake=not logged)
-    hdul = loader(path)
-    note(f"init fits object,{path},{stat()}", verbose > 0)
-    library = crudely_find_library(loader)
-    header = get_header(hdul, hdu_indices[0], library)
-    note(f"got header,{path},{stat()}", verbose > 1)
-    # TODO: this is a slightly weird hack to revert astropy's automatic
-    #  translation of some FITS header values to astropy types. There might
-    #  be a cleaner way to do this.
-    if library == "astropy":
-        output_header = {}
-        for k, v in header.items():
-            if isinstance(v, (str, float, int)):
-                output_header[k] = v
-            else:
-                output_header[k] = str(v)
-        header = output_header
-    output = {'header': header, 'stat': stat}
-    file_attr = next(filter(lambda attr: "filename" in attr, dir(hdul)))
-    output['path'] = getattr(hdul, file_attr)
-    if isinstance(output['path'], Callable):
-        output['path'] = output['path']()
-    if get_handles is True:
-        # initialize selected HDU object and get its data 'handles'
-        output["handles"] = [hdul[hdu_ix] for hdu_ix in hdu_indices]
-        # fitsio exposes slices on HDU data by assigning a __getitem__ method
-        # directly to its HDU objects. astropy instead assigns __getitem__
-        # methods to attributes of HDU objects, so here we return an attribute
-        # rather than the HDU itself as the "handle". by default this is
-        # "data", but there are other attributes, notably "section", that also
-        # offer data access
-        if library == "astropy":
-            output["handles"] = [
-                getattr(h, astropy_handle_attribute) for h in output["handles"]
-            ]
-        # TODO: section case
-        note(f"got data handles,{path},{stat()}", loud=verbose > 1)
-    if get_wcs is True:
-        output['wcs'] = astropy.wcs.WCS(extract_wcs_keywords(header))
-        note(f"initialized wcs,{path},{stat()}", loud=verbose > 1)
-    output['log'] = note(None, eject=True)
-    return output
