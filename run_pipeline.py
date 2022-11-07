d1ab831b61e6689abcea0ae689538b4ee44438c2
[bekahalbach] <[bekah.albach@gmail.com]>
2022-11-07 09:58:49 -0600

    pipeline run changes

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,7 +2,7 @@ from gPhoton.pipeline import execute_pipeline
 
 if __name__ == "__main__":
     execute_pipeline(
-        7330,
+        23456,
         "NUV",
         depth=None,
         threads=4,
@@ -10,9 +10,10 @@ if __name__ == "__main__":
         remote_root="/mnt/s3",
         recreate=False,
         aperture_sizes=[12.8, 51.2],
-        write={"movie": False, "image": True},
-        coregister_lightcurves=False,
+        write={"movie": True, "image": True},
+        coregister_lightcurves=True,
         compression="rice",
         lil=True,
-        photometry_only=False
+        photometry_only=False,
+        verbose=4
     )
