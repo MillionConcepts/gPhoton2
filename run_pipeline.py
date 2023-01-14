7b76d02a13cc2adc898857edacb51b8ef3e59bf4
michael <mstclair@millionconcepts.com>
2023-01-14 13:47:15 -0500

    mess w/default script parameters

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,18 +2,30 @@ from gPhoton.pipeline import execute_pipeline
 
 if __name__ == "__main__":
     execute_pipeline(
-        781,
+        23456,
         "NUV",
         depth=60,
+        # integer; None to deactivate (default None)
         threads=4,
+        # where to both write output data and look for input data
         local_root="test_data",
+        # auxiliary remote location for input data
+        # remote_root="/mnt/s3",
         recreate=True,
+        # list of floats; relevant only to lightcurve / photometry portion
         aperture_sizes=[12.8],
+        # actually write image/movie products? otherwise hold in memory but
+        # discard (possibly after performing photometry).
         write={"movie": True, "image": True},
         coregister_lightcurves=False,
-        stop_after="moviemaker",
+        # photonpipe, moviemaker, None (default None)
+        stop_after=None,
         photometry_only=False,
+        # None, "gzip", "rice"
         compression="rice",
+        # use array sparsification on movie frames?
         lil=True,
-        burst=True
+        # write movie frames as separate files
+        burst=True,
+        extended_photonlist=False
     )
