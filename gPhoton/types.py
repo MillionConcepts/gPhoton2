b207204ac88afa696b2468fa46c6070328b49985
Zack Weinberg <zack@owlfolio.org>
2025-03-27 11:41:34 -0400

    Define type variables in types.py for standard ways of using NumPy.
    Our low-level code generally expects a bunch of NumPy array and
    scalar arguments that all have the same dtype, but it doesn’t
    particularly care *which dtype* that is.  Introduce type variables
    to make annotating for this usage easier.
    
    I’m not sure the way I’m writing these is 100% accurate but it seems
    to work (as used in subsequent commits).
    
    Note that it is not currently possible to specify the expected *shape*
    of an ndarray; there is a plan to make this possible (see PEP 646) but
    it is not yet implemented in NumPy itself.

--- gPhoton/types.py
+++ gPhoton/types.py
@@ -1,5 +1,35 @@
 from pathlib import Path
-from typing import Literal
+from typing import Any, Literal, TypeVar
+
+import numpy as np
+from numpy.typing import NDArray
 
 Pathlike = str | Path
 GalexBand = Literal["NUV", "FUV"]
+
+
+# Type variables for code that is generic over numpy dtype as long as
+# all the arguments (or groups of the arguments) have consistent dtype.
+# For typical usage patterns see gPhoton/vorpal.py and
+# gPhoton/coords/gnomonic.py.
+NFloat = TypeVar("NFloat", bound=np.floating[Any])
+NFloatX = TypeVar("NFloatX", bound=np.floating[Any])
+NFloatY = TypeVar("NFloatY", bound=np.floating[Any])
+
+NInt = TypeVar("NInt", bound=np.integer[Any])
+NIntX = TypeVar("NIntX", bound=np.integer[Any])
+NIntY = TypeVar("NIntY", bound=np.integer[Any])
+
+NNum = TypeVar("NNum", bound=np.number[Any])
+NNumX = TypeVar("NNumX", bound=np.number[Any])
+NNumY = TypeVar("NNumY", bound=np.number[Any])
+
+
+__all__ = [
+    "Pathlike",
+    "GalexBand",
+    "NDArray",
+    "NFloat", "NFloatX", "NFloatY",
+    "NInt", "NIntX", "NIntY",
+    "NNum", "NNumX", "NNumY",
+]
