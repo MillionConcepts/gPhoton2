9ab3a80d94d0e0d651f27e41247ea0633f0d8cef
Zack Weinberg <zack@owlfolio.org>
2025-03-27 13:08:58 -0400

    Add full type annotations to vorpal.py.
    Also switch a use of one-argument numpy.where to numpy.nonzero as
    recommended by the numpy manual.

--- gPhoton/vorpal.py
+++ gPhoton/vorpal.py
@@ -2,15 +2,26 @@
 numba-compiled fast ndarray-partitioning methods
 """
 
-from numba import njit
 import numpy as np
 
-
-@njit(cache=True)
-def between(reference, t0, t1):
-    return np.where((reference >= t0) & (reference < t1))
+from gPhoton.numba_utilz import jit
+from gPhoton.types import NDArray, NNumX, NNumY
 
 
-@njit(cache=True)
-def slice_between(subject, reference, t0, t1):
+@jit
+def between(
+    reference: NDArray[NNumX],
+    t0: NNumX,
+    t1: NNumX,
+) -> tuple[NDArray[np.intp], ...]:
+    return np.nonzero((reference >= t0) & (reference < t1))
+
+
+@jit
+def slice_between(
+    subject: NDArray[NNumY],
+    reference: NDArray[NNumX],
+    t0: NNumX,
+    t1: NNumX,
+) -> NDArray[NNumY]:
     return subject[between(reference, t0, t1)]
